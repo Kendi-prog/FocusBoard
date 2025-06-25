b771e8245ca6313883bbdffba19f11ebb38c6685
Kendi <muriungikendileila@gmail.com>
2025-06-25 14:55:10 +0300

    improving the todo list items by changing the completed feature and removing the update and delete buttons to icons + ARIA labels

--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -1,3 +1,5 @@
+import { PencilIcon , TrashIcon} from "@heroicons/react/24/outline";
+
 const TodoList = ({ myTodos, handleDeleteTodo, handleUpdate }) => {
   return (
     <div>
@@ -11,20 +13,25 @@ const TodoList = ({ myTodos, handleDeleteTodo, handleUpdate }) => {
               <span className="font-semibold mr-2"> Name:</span> {todo.todoName}
             </h1>
             <h1>
-              <span className="font-semibold mr-2">Desc: </span> {todo.desc}
+              <span className="font-semibold mr-2">Description: </span> {todo.desc}
             </h1>
-            <div>{todo.completed && "✅"}</div>
+            <div>  
+              {todo.completed && (
+                <span className="mt-2 inline-block bg-green-100 text-green-800 text-xs font-medium px-2 py-1 rounded-full">
+                  ✓ Completed
+                </span>
+              )}
+            </div>
           </div>
-          <div className="flex gap-2 items-center">
-            <button className="p-2 bg-blue-600 rounded-md text-white" onClick={() => handleUpdate(todo.id)}>
-              Update
-            </button>
-            <button
-              className="p-2 bg-red-600 rounded-md text-white hover:cursor-pointer"
-              onClick={() => handleDeleteTodo(todo.id)}
-            >
-              Delete
-            </button>
+          <div className="flex gap-4 items-center">
+              <button onClick={() => handleUpdate(todo.id)} title="Edit" aria-label="Edit">
+                <PencilIcon className="w-5 h-5 text-blue-600 hover:text-blue-800" />
+              </button>
+
+              <button onClick={() => handleDeleteTodo(todo.id)} title="Delete" aria-label="Delete">
+                <TrashIcon className="w-5 h-5 mr-2 text-red-600 hover:text-red-800" />
+              </button>
+
           </div>
         </div>
       ))}
