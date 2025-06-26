b71d6005b0c0c3dafe7f52a8746025e4454a9eda
Kendi <muriungikendileila@gmail.com>
2025-06-27 02:51:08 +0300

    deleting files

--- src/components/filteredTodos/notStarted.jsx
+++
@@ -1,9 +0,0 @@
-import useTodos from "../../context/useTodos";
-import TodoList from "../todos/TodoList";
-
-export default function NotStarted() {
-  const { todos } = useTodos();
-  const filtered = todos.filter(todo => todo.status === "not-started");
-
-  return <TodoList myTodos={filtered} />;
-}
