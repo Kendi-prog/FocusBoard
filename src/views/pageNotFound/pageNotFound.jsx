f24f1cc8fb4a859205b15481c40aaf80fc1c2202
Kendi <muriungikendileila@gmail.com>
2025-06-30 07:55:59 +0300

    changing the Go back home button from a Link to using useNavigate

--- src/views/pageNotFound/pageNotFound.jsx
+++ src/views/pageNotFound/pageNotFound.jsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import notFoundPage from "../../assets/404-vector1.png"
 
 export default function PageNotFound() {
+    const navigate = useNavigate();
+
     return (
         <div className="min-h-screen flex flex-col items-center text-center px-4 ">
             <img
@@ -11,8 +13,11 @@ export default function PageNotFound() {
             />
             {/* <h1 className="text-4xl font-bold text-[#1e3a8a] mb-2">404 - Page Not Found</h1> */}
             <h1 className="text-4xl font-bold text-[#1e3a8a] mb-2">Ooops, the page you are looking for does not exist.</h1>
-            <Link className="inline-block border-2 border-blue-500 text-blue-500 px-6 py-3 rounded-full hover:bg-blue-500 hover:text-white hover:shadow-lg hover:shadow-blue-400 transition-all duration-200"
-            to="/">Go Back Home</Link>
+            <button 
+            className="inline-block border-2 border-blue-500 text-blue-500 px-6 py-3 rounded-full hover:bg-blue-500 hover:text-white hover:shadow-lg hover:shadow-blue-400 transition-all duration-200"
+            onClick={() => navigate("/")}>
+                Go Back Home
+            </button>
         </div>
     )
 }
